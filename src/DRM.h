9c1fd3fdebae730e2546b5023bc8e2d8b0e685de
ethan706 <ethan07706@gmail.com>
2026-02-05 11:04:50 +0800

    Define CAN configuration constants
    Add DRM_config.h with CAN_BAUD_RATE, CAN_MAX_NODES, CAN_UPDATE_INTERVAL

--- src/DRM.h
+++ src/DRM.h
@@ -1,4 +1,6 @@
 #ifndef DRM_H
 #define DRM_H
 
+#include "DRM_config.h"
+
 #endif  // DRM_H
---
+++ src/DRM_config.h
@@ -0,0 +1,12 @@
+#ifndef DRM_CONFIG_H
+#define DRM_CONFIG_H
+
+namespace DRM {
+
+constexpr uint32_t CAN_BAUD_RATE       = 1000000U;
+constexpr uint32_t CAN_MAX_NODES       = 32U;
+constexpr uint32_t CAN_UPDATE_INTERVAL = 1000U;
+
+}  // namespace DRM
+
+#endif  // DRM_CONFIG_H
